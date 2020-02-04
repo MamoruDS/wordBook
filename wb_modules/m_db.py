901ce104f3f2b28a601ffe8c3ab55e80f2da0152
MamoruDS <mamoruds.io@gmail.com>
2020-02-04 14:32:29 +0900

    added methods getWord & addWord
    renamed dbAddBook to addBook
    
    fixed word_id now is a reserved field of book_field

--- wb_modules/m_db.py
+++ wb_modules/m_db.py
@@ -68,7 +68,52 @@ def recTableCheck():
     tableCheck('card_records', TABLE_REC_OBJ, True, False)
 
 
-def dbAddBook(bookId):
+def addBook(bookId):
     book = m_profile.confGetBook(bookId)
     fields = book[0]['book_fields']
+    fields['word_id'] = {
+        'db_type': 'TEXT',
+        'field_unique': True,
+        'field_render_tag': 'text'
+    }
     tableCheck(bookId, fields)
+
+
+def addWord(bookId, wordId, wordObj, colNameB64E=True):
+    cols, values = [[], []]
+    wordObj['word_id'] = wordId
+    for col in wordObj.keys():
+        if colNameB64E:
+            cols.append('"{}"'.format(m_utils.b64EncodeStr(col)))
+        values.append('"{}"'.format(wordObj[col]))
+    queryStr = '''
+            INSERT INTO "{}" ({})
+            VALUES
+                ({})
+    '''.format(bookId, ','.join(cols), ','.join(values))
+    cur = CONN.cursor()
+    print(queryStr)
+    cur.execute(queryStr)
+    CONN.commit()
+    wordId = cur.lastrowid
+    # addRec(bookId, wordId)
+    return wordId
+
+
+def getWord(bookId, wordId=None, colNameB64E=True):
+    if wordId is not None:
+        wordId = 'WHERE "d29yZF9pZA=="="{}"'.format(wordId)
+    else:
+        wordId = ''
+    res = CONN.execute('''
+            SELECT * FROM "{}" {}
+    '''.format(bookId, wordId))
+    if colNameB64E:
+        colh = list(map(lambda x: m_utils.b64DecodeStr(x[0]), res.description))
+    else:
+        colh = list(map(lambda x: x[0], res.description))
+    data = [colh]
+    rows = res.fetchall()
+    for row in rows:
+        data.append(list(row))
+    return data
