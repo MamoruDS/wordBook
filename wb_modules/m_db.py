f8de7593ff09feaa4c95c1cde011868f32802871
MamoruDS <mamoruds.io@gmail.com>
2020-02-04 11:52:28 +0900

    added methods recTableCheck & adAddBook

--- wb_modules/m_db.py
+++ wb_modules/m_db.py
@@ -5,11 +5,26 @@ from . import m_utils, m_profile
 DB_NAME = 'data.db'
 TABLE_REC = 'card_records'
 TABLE_REC_OBJ = {
-    'bookId': None,
-    'wordId': 'INTEGER',
-    'lcId': 'TEXT',
-    'level': 'INTEGER',
-    'nextTS': 'INTEGER'
+    'bookId': {
+        'db_type': 'TEXT',
+        'field_unique': False
+    },
+    'wordId': {
+        'db_type': 'INTEGER',
+        'field_unique': False
+    },
+    'lcId': {
+        'db_type': 'INTEGER',
+        'field_unique': False
+    },
+    'level': {
+        'db_type': 'INTEGER',
+        'field_unique': False
+    },
+    'nextTS': {
+        'db_type': 'INTEGER',
+        'field_unique': False
+    }
 }
 SQL_TYPE = set(['NULL', 'INTEGER', 'REAL', 'TEXT', 'BLOB'])
 
@@ -34,11 +49,12 @@ def tableCheck(tableName, fields, create=True, colNameB64E=True):
             cols = []
             for col in fields.keys():
                 colType = fields[col]['db_type']
+                unique = fields[col]['field_unique']
                 if colNameB64E:
                     col = m_utils.b64EncodeStr(col)
                 if colType not in SQL_TYPE:
                     colType = 'TEXT'
-                if fields[col]['field_unique']:
+                if unique:
                     cols.append('"{}" {} NOT NULL UNIQUE'.format(col, colType))
                 else:
                     cols.append('"{}" {}'.format(col, colType))
@@ -46,3 +62,13 @@ def tableCheck(tableName, fields, create=True, colNameB64E=True):
             return True
         else:
             return False
+
+
+def recTableCheck():
+    tableCheck('card_records', TABLE_REC_OBJ, True, False)
+
+
+def dbAddBook(bookId):
+    book = m_profile.confGetBook(bookId)
+    fields = book[0]['book_fields']
+    tableCheck(bookId, fields)
