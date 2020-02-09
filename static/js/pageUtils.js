515322173084608bb92210e80f049c75ecde9361
MamoruDS <mamoruds.io@gmail.com>
2020-02-09 15:18:47 +0900

    rewrote method removeElement
    added new method minParser

--- static/js/pageUtils.js
+++ static/js/pageUtils.js
@@ -1,7 +1,7 @@
 const pageUtils = {}
 
 pageUtils.wait = async t => {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
         setTimeout(() => {
             resolve()
         }, t)
@@ -9,30 +9,41 @@ pageUtils.wait = async t => {
 }
 
 pageUtils.removeElement = element => {
-    if (element) try {
-        element.parentNode.removeChild(element)
-    } catch (err) { }
+    if (element == null) {
+        return
+    } else if (element.length) {
+        for (let e of element) {
+            pageUtils.removeElement(e)
+        }
+    } else if (typeof element === 'object') {
+        try {
+            element.parentNode.removeChild(element)
+        } catch (err) { }
+    }
 }
 
 pageUtils.req = async (
-    reqObj = {
+    reqOpt = {
         baseURL: undefined,
         url: undefined,
         method: 'get',
         headers: {},
         params: {},
-        data: {}
+        data: {},
     }
 ) => {
-    res = await axios({
-        baseURL: reqObj.baseURL,
-        url: reqObj.url,
-        method: reqObj.method,
-        headers: reqObj.headers,
-        params: reqObj.params,
-        data: reqObj.data,
-        timeout: 500
-    })
+    let res = undefined
+    try {
+        res = await axios({
+            baseURL: reqOpt.baseURL,
+            url: reqOpt.url,
+            method: reqOpt.method,
+            headers: reqOpt.headers,
+            params: reqOpt.params,
+            data: reqOpt.data,
+            timeout: 500,
+        })
+    } catch (e) { }
     return res
 }
 
@@ -43,6 +54,35 @@ pageUtils.getDefaultReqOptions = () => {
         method: 'get',
         headers: {},
         params: {},
-        data: {}
+        data: {},
+    }
+}
+
+pageUtils.minParser = (min) => {
+    let t = min
+    if (t <= 55) {
+        return `${t} mins`
+    } else {
+        t = (t / 60).toFixed(1)
+        if (t <= 23) {
+            return `${t} hrs`
+        } else {
+            t = (t / 24).toFixed(1)
+            if (t <= 6) {
+                return `${t} dys`
+            } else {
+                t = (t / 7).toFixed(1)
+                if (t < 4) {
+                    return `${t} wks`
+                } else {
+                    t = (min / 60 / 24 / 30).toFixed(1)
+                    if (t < 12) {
+                        return `${t} mos`
+                    } else {
+                        return `${(t / 12).toFixed(1)} yrs`
+                    }
+                }
+            }
+        }
     }
 }
